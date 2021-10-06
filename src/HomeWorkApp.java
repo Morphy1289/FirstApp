e0468c5caceb0bc45aa145a88720fe61b8b1e8ad
Dima <Dimam29@yandex.ru>
2021-10-06 22:05:15 +0300

    HomeWork for lesson1

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,56 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+
+    }
+    public static void printThreeWords(){
+        System.out.println("""
+                Orange\s
+                Banana\s
+                Apple""");
+
+    }
+    public static void checkSumSign() {
+        int a = 2;
+        int b = 2;
+        if ((a + b) > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+    public static void printColor() {
+        int value = 3;
+
+        if (value <= 0){
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value >= 100){
+            System.out.println("Зелёный");
+        }
+
+    }
+    public static void compareNumbers(){
+        int a = 2;
+        int b = 3;
+
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+
+
+    }
+
+}
+
